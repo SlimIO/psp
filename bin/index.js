bff75162d49bc6c531edca2e37ab9be5519d91c4
Nicolas Marteau <nico_mart@hotmail.fr>
2019-03-27 11:48:59 +0100

    refactor: adding obj Reflect moving a few lines

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
 // Require Node.js Dependencies
-
 const { readdir, readFile } = require("fs").promises;
 const { join } = require("path");
 
 // Require Third-party Dependencies
-
 const { red, green, yellow, bold, black, bgBlack, white } = require("kleur");
 const inquirer = require("inquirer");
 
 // Constants
-
-const pathMainDir = join(process.cwd(), "/test");
+const pathMainDir = join(process.cwd(), "test");
 const requiredFiles = require("../src/requiredFiles.json");
+const excludeFiles = new Set(["package.json", "LICENSE"]);
+const msgNotExist = "doesn't exist in your main directory.";
 
 const requiredDir = {
     src: "",
@@ -36,26 +35,28 @@ async function main(MainDir) {
     for (const file of requiredFiles) {
         // If file exist
         if (elemMainDir.has(file.name)) {
-            // Read file
-            const excludeFiles = new Set(["package.json", "LICENSE"]);
             let contentUserFile = "";
             let contentLocalFile = "";
-            // If file isn't excluded
+
+            // If file isn't excluded, read file
             if (!excludeFiles.has(file.name)) {
-                contentUserFile = await readFile(join(MainDir, `/${file.name}`), { encoding: "utf8" });
+                contentUserFile = await readFile(join(MainDir, file.name), { encoding: "utf8" });
             }
+
             // Switch all files
             switch (file.name) {
-                case ".eslintrc":
-                    if (JSON.parse(contentUserFile).extends !== file.content[0].extends) {
+                case ".eslintrc": {
+                    const ctntUsFileJson = JSON.parse(contentUserFile);
+                    if (ctntUsFileJson.extends !== file.content[0].extends) {
                         console.log(bold().red("CRITICAL :"), green(file.name), file.message[0]);
                     }
-                    if (JSON.parse(contentUserFile).rules !== undefined) {
+                    if (Reflect.has(ctntUsFileJson, "rules")) {
                         console.log(yellow("WARNING :"), green(file.name), file.message[1]);
                     }
                     break;
+                }
                 case ".editorconfig":
-                    contentLocalFile = await readFile(join(__dirname, "../src/.editorconfig"), { encoding: "utf8" });
+                    contentLocalFile = await readFile(join(__dirname, "..", "template", ".editorconfig"), { encoding: "utf8" });
                     if (contentUserFile !== contentLocalFile) {
                         console.log(yellow("WARNING :"), green(file.name), file.message[0]);
                     }
@@ -69,13 +70,13 @@ async function main(MainDir) {
             }
             continue;
         }
+
         // If file doesn't exist
-        const message = "doesn't exist in your main directory.";
         if (file.name === "index.d.ts") {
-            console.log(yellow("WARNING :"), green(file.name), message);
+            console.log(yellow("WARNING :"), green(file.name), msgNotExist);
         }
         else {
-            console.log(bold().red("CRITICAL :"), green(file.name), message);
+            console.log(bold().red("CRITICAL :"), green(file.name), msgNotExist);
         }
     }
 }
