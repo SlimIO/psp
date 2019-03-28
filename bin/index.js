eebd89b877d196cbcf781c3d7be4e8a7f4689e35
Nicolas Marteau <nico_mart@hotmail.fr>
2019-03-28 16:38:38 +0100

    chore: Switch case for CLI projects

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,7 @@ const { join } = require("path");
 // Require Third-party Dependencies
 const { red, green, yellow, bold, black, bgBlack, white } = require("kleur");
 const inquirer = require("inquirer");
+const Manifest = require("@slimio/manifest");
 
 // Constants
 const pathMainDir = join(process.cwd(), "test");
@@ -25,20 +26,8 @@ const msgNotExist = "doesn't exist in your main directory.";
  */
 
 async function main(MainDir) {
+    // Read the main directory of user
     const elemMainDir = new Set(await readdir(MainDir));
-    // If slimio manisfest doesn't installed in this project
-    if (!elemMainDir.has("include") && !elemMainDir.has("bin")) {
-        console.log(bold().red("CRITICAL :"), "You must install the slimio manifest -", green("npm i @slimio/manifest"));
-        console.log(`
-        ${green(".toml")} file must be created 
-        at the root of the project to determine if 
-        your application is CLI or N-API. Go to this 
-        link to install this file :
-        https://github.com/SlimIO/Manifest
-        `);
-
-        return;
-    }
 
     // Loop on required files array
     for (const file of requiredFiles) {
@@ -58,7 +47,7 @@ async function main(MainDir) {
                 case ".eslintrc": {
                     const ctntUsFileJson = JSON.parse(contentUserFile);
                     if (ctntUsFileJson.extends !== file.extends) {
-                        console.log(bold().red("CRITICAL :"), green(file.name), file.message[0]);
+                        console.log(red("CRITICAL :"), green(file.name), file.message[0]);
                     }
                     if (Reflect.has(ctntUsFileJson, "rules")) {
                         console.log(yellow("WARNING :"), green(file.name), file.message[1]);
@@ -75,7 +64,7 @@ async function main(MainDir) {
                 // .commitlint.config.js
                 case "commitlint.config.js":
                     if (!contentUserFile.indexOf("['@commitlint/config-conventional']")) {
-                        console.log(bold().red("CRITICAL :"), green(file.name), file.message);
+                        console.log(red("CRITICAL :"), green(file.name), file.message);
                     }
                     break;
                 default:
@@ -88,8 +77,70 @@ async function main(MainDir) {
             console.log(yellow("WARNING :"), green(file.name), msgNotExist);
         }
         else {
-            console.log(bold().red("CRITICAL :"), green(file.name), msgNotExist);
+            console.log(red("CRITICAL :"), green(file.name), msgNotExist);
         }
+    }
+
+    // If slimio.toml exists
+    if (elemMainDir.has("slimio.toml")) {
+        manifest = Manifest.open().toJSON();
+        switch (manifest.type) {
+            // CLI
+            case "CLI": {
+                const folder = yellow("bin folder");
+                const file = yellow("index.js file");
+
+                // If the main directory content a bin folder
+                if (elemMainDir.has("bin")) {
+                    try {
+                        const ctnIndexFile = await readFile(join(MainDir, "bin", "index.js"));
+                        // eslint-disable-next-line max-depth
+                        if (ctnIndexFile.indexOf("#!/usr/bin/env node")) {
+                            console.log(yellow("WARNING :"), `The ${file} must content => #!/usr/bin/env node <= to first line`);
+                            break;
+                        }
+                    }
+                    catch (error) {
+                        console.log(yellow("WARNING :"), `Impossible to found ${file} to ${folder}`);
+                        break;
+                    }
+                }
+
+                // Or not
+                else {
+                    console.log(red("CRITICAL :"), `Your project is a CLI and must content a ${folder} with a ${file} !`);
+                    break;
+                }
+                // preferGlobal, bin, main in package.json
+                console.log(`
+                Into your package.json, think about :
+                
+                preferGlobal: true
+                bin: { your-project-name: ./bin/index.js}
+                see documentation to https://docs.npmjs.com/files/package.json#bin
+                main: ./bin/index.js
+                `);
+                break;
+            }
+            // N-API
+            case "N-API":
+                break;
+            default:
+        }
+    }
+
+    // If slimio manisfest doesn't installed in this project => return
+    else {
+        console.log(red("CRITICAL :"), "You must install the slimio manifest -", green("npm i @slimio/manifest"));
+        console.log(`
+        ${green(".toml")} file must be created 
+        at the root of the project to determine if 
+        your application is CLI or N-API. Go to this 
+        link to install this file :
+        https://github.com/SlimIO/Manifest
+        `);
+
+        return;
     }
 }
 
