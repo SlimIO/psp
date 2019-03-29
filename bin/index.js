a601ccf76b1c647eec4bd00f2d50caad3f347f99
Nicolas Marteau <nico_mart@hotmail.fr>
2019-03-29 19:05:05 +0100

    chore: change block ifelse with simple if and continue

--- bin/index.js
+++ bin/index.js
@@ -10,12 +10,14 @@ const emoji = require("node-emoji");
 const { green } = require("kleur");
 const Manifest = require("@slimio/manifest");
 
-// Constants
-const pathMainDir = process.cwd();
+// Require Internal Dependencies
 const requiredFiles = require("../src/requiredFiles.json");
 const msg = require("../src/messages.js");
-const requiredDir = new Set(["src", "test", "benchmark", "docs"]);
-const excludeFiles = new Set(["package.json", "LICENSE"]);
+
+// Constants
+const PATH_MAIN_DIR = process.cwd();
+const REQUIRE_DIR = new Set(["src", "test", "benchmark", "docs"]);
+const EXCLUDE_FILES = new Set(["package.json", "LICENSE"]);
 const E_SEV = Object.freeze({
     CRIT: ":no_entry:",
     WARN: ":warning",
@@ -43,64 +45,62 @@ function log(severity, message, file) {
     }
 }
 
-async function main(MainDir) {
+async function main() {
     // Read the main directory of user
-    const elemMainDir = new Set(await readdir(MainDir));
+    const elemMainDir = new Set(await readdir(PATH_MAIN_DIR));
 
     // Loop on required files array
     for (const file of requiredFiles) {
-        // If file exist
-        if (elemMainDir.has(file.name)) {
-            let contentUserFile = "";
-            let contentLocalFile = "";
-
-            // If file isn't excluded, read file
-            if (!excludeFiles.has(file.name)) {
-                contentUserFile = await readFile(join(MainDir, file.name), { encoding: "utf8" });
+        // If file not exist
+        if (!elemMainDir.has(file.name)) {
+            // If file doesn't exist
+            if (file.name === "index.d.ts") {
+                log(E_SEV.WARN, msg.fileNotExist, file.name);
+                continue;
             }
-
-            // Switch all files
-            switch (file.name) {
-                // .eslintrc
-                case ".eslintrc": {
-                    const ctntUsFileJson = JSON.parse(contentUserFile);
-                    if (ctntUsFileJson.extends !== file.extends) {
-                        log(E_SEV.CRIT, msg.eslintExtends, file.name);
-                    }
-                    if (Reflect.has(ctntUsFileJson, "rules")) {
-                        log(E_SEV.WARN, msg.eslintRulesKey, file.name);
-                    }
-                    break;
-                }
-                // .editorconfig
-                case ".editorconfig":
-                    contentLocalFile = await readFile(join(__dirname, "..", "template", ".editorconfig"), { encoding: "utf8" });
-                    if (contentUserFile !== contentLocalFile) {
-                        log(E_SEV.WARN, msg.editorConf, file.name);
-                    }
-                    break;
-                // .commitlint.config.js
-                case "commitlint.config.js":
-                    if (!contentUserFile.indexOf("['@commitlint/config-conventional']")) {
-                        log(E_SEV.CRIT, msg.commitLint, file.name);
-                    }
-                    break;
-                default:
+            else {
+                log(E_SEV.CRIT, msg.fileNotExist, file.name);
             }
+        }
+        // If file is excluded
+        if (EXCLUDE_FILES.has(file.name)) {
             continue;
         }
+        // Read file
+        const contentUserFile = await readFile(join(PATH_MAIN_DIR, file.name), { encoding: "utf8" });
 
-        // If file doesn't exist
-        if (file.name === "index.d.ts") {
-            log(E_SEV.WARN, msg.fileNotExist, file.name);
-        }
-        else {
-            log(E_SEV.CRIT, msg.fileNotExist, file.name);
+        // Switch all files
+        switch (file.name) {
+            // .eslintrc
+            case ".eslintrc": {
+                const ctntUsFileJson = JSON.parse(contentUserFile);
+                if (ctntUsFileJson.extends !== file.extends) {
+                    log(E_SEV.CRIT, msg.eslintExtends, file.name);
+                }
+                if (Reflect.has(ctntUsFileJson, "rules")) {
+                    log(E_SEV.WARN, msg.eslintRulesKey, file.name);
+                }
+                break;
+            }
+            // .editorconfig
+            case ".editorconfig":
+                contentLocalFile = await readFile(join(__dirname, "..", "template", ".editorconfig"), { encoding: "utf8" });
+                if (contentUserFile !== contentLocalFile) {
+                    log(E_SEV.WARN, msg.editorConf, file.name);
+                }
+                break;
+            // .commitlint.config.js
+            case "commitlint.config.js":
+                if (!contentUserFile.indexOf("['@commitlint/config-conventional']")) {
+                    log(E_SEV.CRIT, msg.commitLint, file.name);
+                }
+                break;
+            default:
         }
     }
 
     // Folder management
-    for (const dir of requiredDir) {
+    for (const dir of REQUIRE_DIR) {
         if (!elemMainDir.has(dir)) {
             switch (dir) {
                 case "src":
@@ -115,58 +115,55 @@ async function main(MainDir) {
         }
     }
 
-    // If slimio.toml exists for projects structure
-    if (elemMainDir.has("slimio.toml")) {
-        const manifest = Manifest.open().toJSON();
-        switch (manifest.type) {
-            // CLI
-            case "CLI": {
-                // If the main directory content a bin folder
-                if (elemMainDir.has("bin")) {
-                    try {
-                        const ctnIndexFile = await readFile(join(MainDir, "bin", "index.js"));
-                        // eslint-disable-next-line max-depth
-                        if (ctnIndexFile.indexOf("#!/usr/bin/env node")) {
-                            log(E_SEV.WARN, msg.shebang);
-                            break;
-                        }
-                    }
-                    catch (error) {
-                        log(E_SEV.CRIT, msg.indexJsNotExist);
-                    }
-                }
-
-                // Or not
-                else {
-                    log(E_SEV.CRIT, msg.binNotExist);
-                }
-                // preferGlobal, bin, main in package.json
-                console.log(msg.rootFieldsCLI);
-                break;
-            }
-            // N-API
-            case "NAPI":
-                // If include folder doesn't exist.
-                if (!elemMainDir.has("include")) {
-                    log(E_SEV.CRIT, msg.napiInclude);
-                }
-
-                // If binding.gyp file doesn't exist
-                if (!elemMainDir.has("binding.gyp")) {
-                    log(E_SEV.CRIT, msg.napiBinding);
-                }
-
-                // gypfile in package.json
-                console.log(msg.rootFieldsNAPI);
-                break;
-            default:
-        }
+    // If slimio manisfest doesn't installed in this project, exit
+    if (!elemMainDir.has("slimio.toml")) {
+        log(E_SEV.CRIT, msg.manifest);
     }
 
-    // If slimio manisfest doesn't installed in this project => return
-    else {
-        log(E_SEV.CRIT, msg.manifest);
+    // If slimio.toml exists for projects structure
+    const manifest = Manifest.open().toJSON();
+    switch (manifest.type) {
+        // CLI
+        case "CLI": {
+            // If the main directory content a bin folder
+            if (elemMainDir.has("bin")) {
+                try {
+                    const ctnIndexFile = await readFile(join(PATH_MAIN_DIR, "bin", "index.js"));
+                    if (ctnIndexFile.indexOf("#!/usr/bin/env node")) {
+                        log(E_SEV.WARN, msg.shebang);
+                        break;
+                    }
+                }
+                catch (error) {
+                    log(E_SEV.CRIT, msg.indexJsNotExist);
+                }
+            }
+
+            // Or not
+            else {
+                log(E_SEV.CRIT, msg.binNotExist);
+            }
+            // preferGlobal, bin, main in package.json
+            console.log(msg.rootFieldsCLI);
+            break;
+        }
+        // N-API
+        case "NAPI":
+            // If include folder doesn't exist.
+            if (!elemMainDir.has("include")) {
+                log(E_SEV.CRIT, msg.napiInclude);
+            }
+
+            // If binding.gyp file doesn't exist
+            if (!elemMainDir.has("binding.gyp")) {
+                log(E_SEV.CRIT, msg.napiBinding);
+            }
+
+            // gypfile in package.json
+            console.log(msg.rootFieldsNAPI);
+            break;
+        default:
     }
 }
 
-main(pathMainDir).catch(console.error);
+main().catch(console.error);
