85c4c62036cc82b23424cca0246ad592abbe9b6b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-14 22:59:08 +0200

    refactor: replace @slimio/arg-parser with sade

--- bin/index.js
+++ bin/index.js
@@ -4,56 +4,36 @@
 require("make-promises-safe");
 
 // Require Third-party Dependencies
-const { argDefinition, parseArg, help } = require("@slimio/arg-parser");
 const { cyan, yellow, white } = require("kleur");
+const sade = require("sade");
 
+// Require Internal Dependencies
 const { readFileLocal } = require("../src/utils.js");
 const psp = require("../index.js");
 
-/**
- * @async
- * @function main
- * @returns {Promise<void>}
- */
-async function main() {
-    let forceMode = false;
-    {
-        const argDefs = [
-            argDefinition("-h --help", "Show help"),
-            argDefinition("-v --version", "Show version"),
-            argDefinition("--gitignore", "show gitignore help"),
-            argDefinition("--editorconfig", "show editorconfig help"),
-            argDefinition("--npmignore", "show npmignore help"),
-            argDefinition("--force", "Enable force mode")
-        ];
+sade("psp", true)
+    .version("0.5.0")
+    .option("--gitignore", "show gitignore help", false)
+    .option("--editorconfig", "show editorconfig help", false)
+    .option("--npmignore", "show npmignore help", false)
+    .option("--force", "enable force mode", false)
+    .action(async(opts) => {
+        const forceMode = Boolean(opts.force);
+        delete opts.force;
+        delete opts._;
 
-        const argv = parseArg(argDefs);
-        if (argv.get("version")) {
-            console.log(white().bold("version 0.5.0"));
-            process.exit(0);
-        }
-        if (argv.get("help")) {
-            help(argDefs);
-            process.exit(0);
-        }
-
-        forceMode = argv.get("force");
-        argv.delete("help");
-        argv.delete("force");
-        for (const [file, isAsked] of argv.entries()) {
-            if (!isAsked) {
-                continue;
+        for (const [file, bool] of Object.entries(opts)) {
+            if (bool) {
+                console.log("\n---");
+                console.log(`File: .${file}:`);
+                console.log(cyan(await readFileLocal(`.${file}`)));
+                process.exit(0);
             }
-            console.log("\n---");
-            console.log(`File: .${file}:`);
-            console.log(cyan(await readFileLocal(`.${file}`)));
-            process.exit(0);
         }
-    }
 
-    const count = await psp({ forceMode });
-    console.log(
-        white().bold(`\n Finished with: ${yellow().bold(count.crit)} Criticals and ${yellow().bold(count.warn)} Warnings\n`)
-    );
-}
-main().catch(console.error);
+        const count = await psp({ forceMode });
+        console.log(
+            white().bold(`\n Finished with: ${yellow().bold(count.crit)} Criticals and ${yellow().bold(count.warn)} Warnings\n`)
+        );
+    })
+    .parse(process.argv);
