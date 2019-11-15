fb57c3da5bb6f69651fa5ac14bcc8d06e95b0725
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-11-15 22:22:08 +0100

    fix: enhance gitignore difference detection

--- src/files/gitignore.js
+++ src/files/gitignore.js
@@ -1,21 +1,30 @@
 "use strict";
 /* eslint-disable jsdoc/require-jsdoc */
 
+// Require Node.js Dependencies
+const { join } = require("path");
+
 // Require Third-party Dependencies
-const fileNormalize = require("file-normalize");
+const parser = require("file-ignore-parser");
 
 // Require Internal Dependencies
-const { readFileLocal } = require("../utils.js");
 const { editorConf } = require("../messages.js");
 const { WARN } = require("../severities");
 
 // CONSTANTS
 const STR = "\n|   ";
+const TPL_DIR = join(__dirname, "..", "..", "template");
 
-async function execute([fileContent, fileName], log) {
-    const localCtnFile = await readFileLocal(fileName);
+async function execute([, fileName], log, ctx) {
+    const [contentItems, localItems] = await Promise.all([
+        parser(join(ctx.CWD, fileName)),
+        parser(join(TPL_DIR, "gitignore.txt"))
+    ]);
 
-    if (fileNormalize.normalizeEOL(fileContent) !== fileNormalize.normalizeEOL(localCtnFile)) {
+    const intersection = new Set(
+        [...localItems].filter((str) => !contentItems.has(str)));
+
+    if (intersection.size > 0) {
         log(WARN, editorConf.join(STR), fileName);
     }
 }
